d189da2733aad430f61d7c37bedbe4286582119c
Brad Chase <brad@unitary.foundation>
2025-08-19 18:53:09 +0000

    Reduce samples to avoid falling behind on codespace

--- run_bench.sh
+++ run_bench.sh
@@ -5,11 +5,12 @@ compilers=(qiskit pytket cirq ucc pyqpanda)
 
 # Run once to ensure packages are loaded/compiled to bytecode
 for compiler in "${compilers[@]}"; do
+    echo "Dry run $compiler"
     uv run python main.py "$compiler"
 done
 
 # Now run and collect profiling information for each compiler
 for compiler in "${compilers[@]}"; do
-    uv run py-spy record --native -f speedscope -o "${compiler}.profile.json" --rate 1000 -- python main.py "$compiler"
+    uv run py-spy record --native --subprocesses -f speedscope -o "${compiler}.profile.json" -- python main.py "$compiler"
 done
 
